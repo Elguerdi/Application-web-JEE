9cf8c56a2122d898beed00ad249d5cc83c46480e
Elguerdi <ibtissamelguerdi90@gmail.com>
2023-04-27 22:42:44 +0200

    Learn Laravel

---
+++ src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
@@ -0,0 +1,34 @@
+package ma.emsi.patientmvc;
+
+import ma.emsi.patientmvc.entities.Patient;
+import ma.emsi.patientmvc.repositories.PatientRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.CommandLineRunner;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientMvcApplication.class, args);
+    }
+ //   @Bean //Pour que ça s'execute au démarrage
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+        return args -> {
+            //salam
+
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/test/java/com/example/ibtissam/IbtissamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibtissam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbtissamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
